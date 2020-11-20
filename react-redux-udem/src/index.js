0ac3943298905ff21d68a3c0c51c6d5940227a7b
Shadowfax7 <sourabh.srivatsa@shadowfax.in>
2020-11-20 11:27:58 +0530

    initial commit

---
+++ react-redux-udem/src/index.js
@@ -0,0 +1,13 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Create a React Component
+const App = () => {
+    return ( 
+        <div> Hi there!! </div>
+    );
+}
+//Take the React Component and show it on the screen
+ReactDOM.render( <
+    App / > , document.querySelector('#root')
+);
