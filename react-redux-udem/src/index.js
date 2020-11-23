2d3d23e994d541a5bb197b50a7cf2c2f346ea849
Shadowfax7 <sourabh.srivatsa@shadowfax.in>
2020-11-23 12:21:34 +0530

    class based structure

--- react-redux-udem/src/index.js
+++ react-redux-udem/src/index.js
@@ -1,41 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
-const App = () =>{
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <div>
-                    <h4>Warning</h4>
-                    Are you sure you want to Do this?
-                </div>
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:25PM" 
-                comment="Its a Nice Blog" 
-                avatar={faker.image.avatar()}/>
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                author="Alex" 
-                timeAgo="Tomorrow at 7:00PM" 
-                comment="One of the Best Actually!!" 
-                avatar={faker.image.avatar()}/>
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                author="Jane" 
-                timeAgo="Yesterday at 10:00AM" 
-                comment="Yes!! The best blog." 
-                avatar={faker.image.avatar()}/>
-            </ApprovalCard>
-        </div>
-    );
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => {console.log(position)}, 
+//         (error) => {console.log(error)}
+//     );
+//     return (
+//         <div>Lattitude: </div>
+//     );
+// }
+
+class App extends React.Component {
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {console.log(position)}, 
+            (error) => {console.log(error)}
+        );
+
+        return (
+            <div>Lattitude: </div>
+        );
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
